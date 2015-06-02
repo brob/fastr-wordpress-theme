122d55844c562b4d1383cba31485011b1f291101
Bryan Robinson <bryanlrobinson@gmail.com>
2015-06-02 10:08:18 -0500

    Added favicon reference

--- header.php
+++ header.php
@@ -14,8 +14,10 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="icon" href="/images/favicon.png" type="image/x-icon">
+	<link rel="shortcut icon" href="/images/favicon.png" type="image/x-icon">
 
-<?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
